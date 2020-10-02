1d9cec3cea974823beb304ae303e7e1ddc33d559
Michal Michalski <michalskimichaljan@gmail.com>
2020-10-02 09:47:31 +0100

    Changed text in the main description

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 */
 const descriptionPL = 'Programista z <strong class="special">kilkunastoletnim doświadczeniem</strong>, majster elektroniki, <strong class="special">właściciel przyszlego klasyka</strong>. Uwielbiam eksperymentować z nowymi technologiami i zaczynac nowe projekty (ktorych juz jest za duzo).';
 
-const descriptionEN = 'Software developer <strong class="special">with many years of experience</strong>, proficient in electronics, <strong class="special">owner of a future classic car</strong>. I like to experiment with new technologies and start new project (already too many on the go).';
+const descriptionEN = 'Software developer <strong class="special">with many years of experience</strong>, proficient in electronics, <strong class="special">owner of a future classic car</strong>. I like to experiment with new technologies and start exciting new projects (already too many on the go).';
 
 const buttonPL = document.querySelector('.button__PL');
 const buttonEN = document.querySelector('.button__EN');
